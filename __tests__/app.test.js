4f2e14d3379ef3626ac2a11ef3c4771f190f5c04
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-22 14:49:31 -0700

    added return user test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,21 +2,21 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-// const { agent } = require('supertest');
+const { UserService } = require('../lib/services/UserService');
 
 const fakeUser = {
   email: 'bob@bob.com',
   password: '111111',
 };
 
-// const singupSignin = async (userInfo = {}) => {
-//   const password = userInfo.password ?? fakeUser.password;
-//   const auth = request.agent(app);
-//   const user = await UserService.create({ ...fakeUser, ...userInfo });
-//   const { email } = user;
-//   await auth.post('/api/v1/users/sessions').send({ email, password });
-//   return [auth, user];
-// };
+const singupSignin = async (userInfo = {}) => {
+  const password = userInfo.password ?? fakeUser.password;
+  const auth = request.agent(app);
+  const user = await UserService.create({ ...fakeUser, ...userInfo });
+  const { email } = user;
+  await auth.post('/api/v1/users/sessions').send({ email, password });
+  return [auth, user];
+};
 
 describe('user routes', () => {
   beforeEach(() => {
@@ -30,6 +30,17 @@ describe('user routes', () => {
     expect(resp.body).toEqual({
       id: expect.any(String),
       email,
+    });
+  });
+
+  it('should return user', async () => {
+    const [auth, user] = await singupSignin();
+    const person = await auth.get('/api/users/me');
+
+    expect(person.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
     });
   });
 
