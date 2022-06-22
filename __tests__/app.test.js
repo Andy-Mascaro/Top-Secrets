6b13dff69fd7b86a1f7d4b8f52289c3250fbe8d3
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-21 20:01:15 -0700

    added post to test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,24 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const fakeUser = {
+  email: 'bob@bob.com',
+  password: '111111',
+};
+
+describe('user routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('POST should create new user', async () => {
+    const resp = await request(app).post('/api/v1/users').send(fakeUser);
+    const { email, password } = fakeUser;
+
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      email,
+      password,
+    });
   });
   afterAll(() => {
     pool.end();
