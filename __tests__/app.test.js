7880229ccb835f47bfdbad541848ffb3e176493d
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-22 14:13:46 -0700

    found typo in User.js first test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,26 +2,37 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+// const { agent } = require('supertest');
 
 const fakeUser = {
   email: 'bob@bob.com',
   password: '111111',
 };
 
+// const singupSignin = async (userInfo = {}) => {
+//   const password = userInfo.password ?? fakeUser.password;
+//   const auth = request.agent(app);
+//   const user = await UserService.create({ ...fakeUser, ...userInfo });
+//   const { email } = user;
+//   await auth.post('/api/v1/users/sessions').send({ email, password });
+//   return [auth, user];
+// };
+
 describe('user routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
   it('POST should create new user', async () => {
     const resp = await request(app).post('/api/v1/users').send(fakeUser);
-    const { email, password } = fakeUser;
+    const { email } = fakeUser;
 
     expect(resp.body).toEqual({
       id: expect.any(String),
       email,
-      password,
     });
   });
+
   afterAll(() => {
     pool.end();
   });
